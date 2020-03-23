790a71ea7ec18ad8bd7d623ecdcacf6de2be08ad
tongfu <861454910@qq.com>
2020-03-23 17:26:54 +0800

    宽度适配

--- app/src/main/java/com/tongfu/mybrowser/WebActivity.kt
+++ app/src/main/java/com/tongfu/mybrowser/WebActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_web.*
 
@@ -24,6 +25,7 @@ class WebActivity : AppCompatActivity() {
         wv_main.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
         wv_main.settings.loadWithOverviewMode = true
         wv_main.settings.userAgentString = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
+        WebView.setWebContentsDebuggingEnabled(true)
         wv_main.loadUrl(uri)
     }
 
