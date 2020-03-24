80113c5ba945b81fd5e70c76ced8253359b84fd0
tongfu <861454910@qq.com>
2020-03-24 17:28:19 +0800

    增加历史和书签功能（未完成）

--- app/src/main/java/com/tongfu/mybrowser/WebActivity.kt
+++ app/src/main/java/com/tongfu/mybrowser/WebActivity.kt
@@ -20,9 +20,10 @@ class WebActivity : AppCompatActivity() {
         wv_main.settings.javaScriptEnabled = true
         wv_main.webChromeClient = MyWebChromeClient()
         wv_main.webViewClient = MyWebViewClient()
+        wv_main.setInitialScale(50)
         wv_main.settings.setSupportZoom (true)
-        wv_main.settings.useWideViewPort = true
-        wv_main.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+//        wv_main.settings.useWideViewPort = true
+//        wv_main.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
         wv_main.settings.loadWithOverviewMode = true
         wv_main.settings.userAgentString = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
         WebView.setWebContentsDebuggingEnabled(true)
